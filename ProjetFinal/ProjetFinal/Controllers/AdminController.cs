cada0e878278f48dcd7853b3ceacb0a4bba6ded6
Marc-Andre St-Jean <1255342@csimple.org>
2017-11-23 10:19:55 -0500

    Tous les page de l'admin fonctionne

--- ProjetFinal/ProjetFinal/Controllers/AdminController.cs
+++ ProjetFinal/ProjetFinal/Controllers/AdminController.cs
@@ -13,10 +13,10 @@ namespace ProjetFinal.Controllers
         private projetFinalEntities1 projetFinalEntities1 = new projetFinalEntities1();
 
         // GET: Admin
-        public PartialViewResult Index()
-        {
-            return PartialView("Index");
-        }
+        //public PartialViewResult Index()
+        //{
+        //    return PartialView("Index");
+        //}
 
         public PartialViewResult AllUser()
         {
@@ -34,6 +34,24 @@ namespace ProjetFinal.Controllers
         public ActionResult EditXpTable(FormCollection formCollection)
         {
             return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteXpTable(int idXpTable)
+        {
+            xpTable xpTable = projetFinalEntities1.xpTables.ToList().Find(x => x.id == idXpTable);
+            projetFinalEntities1.xpTables.Remove(xpTable);
+            return RedirectToAction("index");
+        }
+
+        public PartialViewResult AddXpTable()
+        {
+            return PartialView();
+        }
+      
+        [HttpPost]
+        public ActionResult AddXpTable(FormCollection formCollection)
+        {
+            return RedirectToAction("AllXpTable");
         }
 
         public PartialViewResult AllXp(int? idXpTable)
@@ -60,18 +78,22 @@ namespace ProjetFinal.Controllers
             //return View(HttpContext.Request.UrlReferrer.AbsoluteUri);
         }
 
-        public ActionResult DeleteXpTable(int idXpTable)
-        {
-            xpTable xpTable = projetFinalEntities1.xpTables.ToList().Find(x => x.id == idXpTable);
-            projetFinalEntities1.xpTables.Remove(xpTable);
-            return RedirectToAction("index");
-        }
-
         public ActionResult DeleteXp(int idXp)
         {
             xp xp = projetFinalEntities1.xps.ToList().Find(x => x.id == idXp);
             projetFinalEntities1.xps.Remove(xp);
             return RedirectToAction("index");
+        }
+
+        public PartialViewResult AddXp()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddXp(FormCollection formCollection)
+        {
+            return RedirectToAction("AllXp");
         }
 
         public PartialViewResult AllSkill()
@@ -80,6 +102,29 @@ namespace ProjetFinal.Controllers
             return PartialView(skill);
         }
 
+        public ActionResult EditSkill()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteSkill(int idSkill)
+        {
+            skill skill = projetFinalEntities1.skills.ToList().Find(x => x.id == idSkill);
+            projetFinalEntities1.skills.Remove(skill);
+            return RedirectToAction("index");
+        }
+
+        public PartialViewResult AddSkill()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddSkill(FormCollection formCollection)
+        {
+            return RedirectToAction("AllSkill");
+        }
+       
         public PartialViewResult AllCompetence(int? idSkill)
         {
             List<competence> competence;
@@ -95,18 +140,27 @@ namespace ProjetFinal.Controllers
             return PartialView(competence);
         }
 
-        public ActionResult DeleteSkill(int idSkill)
+        public ActionResult EditCompentence()
         {
-            skill skill = projetFinalEntities1.skills.ToList().Find(x => x.id == idSkill);
-            projetFinalEntities1.skills.Remove(skill);
-            return RedirectToAction("index");
-        }
+            return View();
+        } 
 
         public ActionResult DeleteCompetence(int idCompentence)
         {
             competence competence = projetFinalEntities1.competences.ToList().Find(x => x.id == idCompentence);
             projetFinalEntities1.competences.Remove(competence);
             return RedirectToAction("index");
+        }
+
+        public PartialViewResult AddCompetence()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult AddCompetence(FormCollection formCollection)
+        {
+            return RedirectToAction("AllCompetence");
         }
     }
 }
