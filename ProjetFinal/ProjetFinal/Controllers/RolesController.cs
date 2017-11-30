c1f9c6cafdf8227c6347fc02132fa1802bdcdb0e
M-A <1255342@csimple.org>
2017-11-30 10:41:22 -0500

    Commentaire

--- ProjetFinal/ProjetFinal/Controllers/RolesController.cs
+++ ProjetFinal/ProjetFinal/Controllers/RolesController.cs
@@ -12,189 +12,230 @@ namespace ProjetFinal.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class RolesController : Controller
-    {        
-            public ActionResult Index()
-            {
-                // Populate Dropdown Lists
-                var context = new ApplicationDbContext();
+    {
+        /// <summary>
+        /// GET: Index
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            // Remplir les dropDownlists
+            var context = new ApplicationDbContext();
 
-                var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
-                new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
-                ViewBag.Roles = rolelist;
+            var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
+            new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = rolelist;
 
-                var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
-                new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
-                ViewBag.Users = userlist;
+            var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
+            new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
+            ViewBag.Users = userlist;
 
-                ViewBag.Message = "";
+            ViewBag.Message = "";
 
-                return View();
-            }
-
-
-            // GET: /Roles/Create
-            public ActionResult Create()
-            {
-                return View();
-            }
-
-            //
-            // POST: /Roles/Create
-            [HttpPost]
-            public ActionResult Create(FormCollection collection)
-            {
-
-                try
-                {
-                    var context = new ApplicationDbContext();
-                    context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
-                    {
-                        Name = collection["RoleName"]
-                    });
-                    context.SaveChanges();
-                    ViewBag.Message = "Role created successfully !";
-                    return RedirectToAction("Index");
-                }
-                catch
-                {
-                    return View();
-                }
-            }
-
-
-            public ActionResult Delete(string RoleName)
-            {
-                var context = new Models.ApplicationDbContext();
-                var thisRole = context.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-                context.Roles.Remove(thisRole);
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            //
-            // GET: /Roles/Edit/5
-            public ActionResult Edit(string roleName)
-            {
-                var context = new Models.ApplicationDbContext();
-                var thisRole = context.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-
-                return View(thisRole);
-            }
-
-            //
-            // POST: /Roles/Edit/5
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
-            {
-                try
-                {
-                    var context = new Models.ApplicationDbContext();
-                    context.Entry(role).State = System.Data.Entity.EntityState.Modified;
-                    context.SaveChanges();
-
-                    return RedirectToAction("Index");
-                }
-                catch
-                {
-                    return View();
-                }
-            }
-
-
-            //  Adding Roles to a user
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public ActionResult RoleAddToUser(string UserName, string RoleName)
-            {
-                var context = new Models.ApplicationDbContext();
-
-                if (context == null)
-                {
-                    throw new ArgumentNullException("context", "Context must not be null.");
-                }
-
-                ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-                userManager.AddToRole(user.Id, RoleName);
-
-
-                ViewBag.Message = "Role created successfully !";
-
-                // Repopulate Dropdown Lists
-                var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
-                ViewBag.Roles = rolelist;
-                var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
-                new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
-                ViewBag.Users = userlist;
-
-                return View("Index");
-            }
-
-
-            //Getting a List of Roles for a User
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public ActionResult GetRoles(string UserName)
-            {
-                if (!string.IsNullOrWhiteSpace(UserName))
-                {
-                    var context = new Models.ApplicationDbContext();
-                    ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-
-                    var userStore = new UserStore<ApplicationUser>(context);
-                    var userManager = new UserManager<ApplicationUser>(userStore);
-                    ViewBag.RolesForThisUser = userManager.GetRoles(user.Id);
-
-
-                    // Repopulate Dropdown Lists
-                    var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
-                    ViewBag.Roles = rolelist;
-                    var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
-                    new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
-                    ViewBag.Users = userlist;
-                    ViewBag.Message = "Roles retrieved successfully !";
-                }
-
-                return View("Index");
-            }
-
-            //Deleting a User from A Role
-            [HttpPost]
-            [ValidateAntiForgeryToken]
-            public ActionResult DeleteRoleForUser(string UserName, string RoleName)
-            {
-                var account = new AccountController();
-                var context = new Models.ApplicationDbContext();
-                ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-
-                var userStore = new UserStore<ApplicationUser>(context);
-                var userManager = new UserManager<ApplicationUser>(userStore);
-
-
-                if (userManager.IsInRole(user.Id, RoleName))
-                {
-                    userManager.RemoveFromRole(user.Id, RoleName);
-                    ViewBag.Message = "Role removed from this user successfully !";
-                }
-                else
-                {
-                    ViewBag.Message = "This user doesn't belong to selected role.";
-                }
-
-                // Repopulate Dropdown Lists
-                var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
-                ViewBag.Roles = rolelist;
-                var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
-                new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
-                ViewBag.Users = userlist;
-
-                return View("Index");
-            }
-
+            return View();
         }
 
 
+        /// <summary>
+        /// GET: /Roles/Create 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// POST: /Roles/Create 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            //Créer un rôle
+            try
+            {
+                var context = new ApplicationDbContext();
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                context.SaveChanges();
+                ViewBag.Message = "Role created successfully !";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        /// <summary>
+        /// GET : Delete 
+        /// </summary>
+        /// <param name="RoleName"></param>
+        /// <returns></returns>
+        public ActionResult Delete(string RoleName)
+        {
+            //Supprimer un Rôle
+            var context = new Models.ApplicationDbContext();
+            var thisRole = context.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            context.Roles.Remove(thisRole);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        /// <summary>
+        /// GET: /Roles/Edit/5 
+        /// </summary>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
+        public ActionResult Edit(string roleName)
+        {
+            //Modifier un Rôle
+            var context = new Models.ApplicationDbContext();
+            var thisRole = context.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            return View(thisRole);
+        }
+
+
+        /// <summary>
+        /// POST: /Roles/Edit/5 
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            //Modifier le rôle d'un utilisateur
+            try
+            {
+                var context = new Models.ApplicationDbContext();
+                context.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        /// <summary>
+        /// POST : Adding Roles to a user 
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <param name="RoleName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        {
+            //Ajouter un rôle a un utilisateur
+            var context = new Models.ApplicationDbContext();
+
+            if (context == null)
+            {
+                throw new ArgumentNullException("context", "Context must not be null.");
+            }
+
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            userManager.AddToRole(user.Id, RoleName);
+
+
+            ViewBag.Message = "Role created successfully !";
+
+            // Remplir les dropDownlists
+            var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = rolelist;
+            var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
+            new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
+            ViewBag.Users = userlist;
+
+            return View("Index");
+        }
+
+
+        /// <summary>
+        /// POST : Getting a List of Roles for a User
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string UserName)
+        {
+            //Rechercher tous les rôle
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                var context = new Models.ApplicationDbContext();
+                ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                ViewBag.RolesForThisUser = userManager.GetRoles(user.Id);
+
+
+                // Remplir les dropDownlists
+                var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = rolelist;
+                var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
+                new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
+                ViewBag.Users = userlist;
+                ViewBag.Message = "Roles retrieved successfully !";
+            }
+
+            return View("Index");
+        }
+
+        /// <summary>
+        /// POST : Deleting a User from A Role 
+        /// </summary>
+        /// <param name="UserName"></param>
+        /// <param name="RoleName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleForUser(string UserName, string RoleName)
+        {
+            //Supprimer un Rôle pour un utilisateur
+            var account = new AccountController();
+            var context = new Models.ApplicationDbContext();
+            ApplicationUser user = context.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+
+            if (userManager.IsInRole(user.Id, RoleName))
+            {
+                userManager.RemoveFromRole(user.Id, RoleName);
+                ViewBag.Message = "Role removed from this user successfully !";
+            }
+            else
+            {
+                ViewBag.Message = "This user doesn't belong to selected role.";
+            }
+
+            // Remplir les dropDownlists
+            var rolelist = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = rolelist;
+            var userlist = context.Users.OrderBy(u => u.UserName).ToList().Select(uu =>
+            new SelectListItem { Value = uu.UserName.ToString(), Text = uu.UserName }).ToList();
+            ViewBag.Users = userlist;
+
+            return View("Index");
+        }
     }
+}
--- ProjetFinal/ProjetFinal/Controllers/UserController.cs
+++ ProjetFinal/ProjetFinal/Controllers/UserController.cs
@@ -12,128 +12,179 @@ namespace ProjetFinal.Controllers
     {
         private projetFinalEntities1 projetFinalEntities1 = new projetFinalEntities1();
 
-        // GET: User
+        /// <summary>
+        /// GET: User
+        /// </summary>
+        /// <returns></returns>
         [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// GET: AllSkill 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult AllSkill()
         {
             List<skill> listSkill = projetFinalEntities1.skills.ToList();
             return View(listSkill);
         }
 
+        /// <summary>
+        /// GET: AllSkill 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Recherche()
         {
             return View();
         }
 
+        /// <summary>
+        /// GET: AllSkill  
+        /// </summary>
+        /// <param name="formCollection"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Recherche(FormCollection formCollection)
         {
+            //La recherche de la différence d'expérience
+
+            //Vider les tempsData juste au cas ou il reste des informations.
             TempData.Remove("errorRechercheUser");
             TempData.Remove("DifXp");
+
             try
             {
-            int id = -1;
+                int id = -1;
 
-            if(formCollection["idRecherche"].Equals(""))
-            {
-                TempData["errorRechercheUser"] = "id vide";
-                return RedirectToAction("Erreur");
-            }
-            else
-            {
-                id = Int32.Parse(formCollection["idRecherche"]);
-            }         
-
-            int findXpTable = (int)projetFinalEntities1.skills.ToList().Find(x => x.id == id).id_xptable;
-
-            int lvlActuel = -1;
-            int xpVoulu = -1;
-            if (formCollection["lvlActuel"].Equals(""))
-            {
-                TempData["errorRechercheUser"] = "lvl Vide";
-                return RedirectToAction("Erreur");
-            }
-            else
-            {
-                lvlActuel = Int32.Parse(formCollection["lvlActuel"]);
-            }
-           
-            if(formCollection["xpVoulu"].Equals(""))
-            {
-                TempData["errorRechercheUser"] = "xp Vide";
-                return RedirectToAction("Erreur");
-            }
-            else
-            {
-                xpVoulu = Int32.Parse(formCollection["xpVoulu"]);
-            }
-          
-
-            int xpMax = (int)projetFinalEntities1.xps.ToList().FindAll(x => x.id_xpTable == findXpTable).Max(x => x.xps);
-            int lvlMax = projetFinalEntities1.xps.ToList().FindAll(x=>x.id_xpTable == findXpTable).Count();
-
-            if(xpVoulu <= 0 && lvlActuel <= 0)
-            {
-                TempData["errorRechercheUser"] = "xp actuel et lvl voulu sont vide";
-                return RedirectToAction("Erreur");
-            }
-            else if(xpVoulu <= 0)
-            {
-                TempData["errorRechercheUser"] = "xp actuel est vide";
-                return RedirectToAction("Erreur");
-            }
-            else if(lvlActuel <= 0)
-            {
-                TempData["errorRechercheUser"] = "lvl actuel est vide";
-                return RedirectToAction("Erreur");
-            }
-
-           
-            if (xpVoulu <= xpMax && lvlActuel <= lvlMax)
-            {
-                List<xp> liste = projetFinalEntities1.xps.ToList().FindAll(x => x.lvl >= lvlActuel);
-                int xpLvl = (int)liste.Find(x => x.id_xpTable == findXpTable).xps;
-                int DifXp = xpVoulu - xpLvl;
-                if (DifXp >= 1)
+                //Vérifier si le textBox idRecherche est null
+                if (formCollection["idRecherche"].Equals(""))
                 {
-                    //https://stackoverflow.com/questions/26474579/viewbag-property-not-displaying-in-view
-                    TempData["DifXp"] = DifXp;
-                    //ViewBag.Ids = id;
+                    //si null il y a une erreur afficher sur la vue
+                    TempData["errorRechercheUser"] = "id vide";
+                    return RedirectToAction("Erreur");
                 }
                 else
                 {
-                    TempData["errorRechercheUser"] = "La valeur de xp voulu est plus grand que le lvl de départ";
+                    id = Int32.Parse(formCollection["idRecherche"]);
+                }
+
+                int findXpTable = (int)projetFinalEntities1.skills.ToList().Find(x => x.id == id).id_xptable;
+
+                int lvlActuel = -1;
+                int xpVoulu = -1;
+
+                //Vérifier si le textBox lvlActuel est null
+                if (formCollection["lvlActuel"].Equals(""))
+                {
+                    //si null il y a une erreur afficher sur la vue
+                    TempData["errorRechercheUser"] = "lvl Vide";
                     return RedirectToAction("Erreur");
                 }
-            }
-            else
-            {
-                TempData["errorRechercheUser"] = "Les chiffres entres sont trop grand pour les niveaus ou experiences";
-                return RedirectToAction("Erreur");
-            }
+                else
+                {
+                    lvlActuel = Int32.Parse(formCollection["lvlActuel"]);
+                }
 
-            return RedirectToAction("Calcul", new {id = id});
+                //Vérifier si le textBox xpVoulu est null
+                if (formCollection["xpVoulu"].Equals(""))
+                {
+                    //si null il y a une erreur afficher sur la vue
+                    TempData["errorRechercheUser"] = "xp Vide";
+                    return RedirectToAction("Erreur");
+                }
+                else
+                {
+                    xpVoulu = Int32.Parse(formCollection["xpVoulu"]);
+                }
+
+                //Recherche dans les tables les valeurs maximum pour xp et lvl
+                int xpMax = (int)projetFinalEntities1.xps.ToList().FindAll(x => x.id_xpTable == findXpTable).Max(x => x.xps);
+                int lvlMax = projetFinalEntities1.xps.ToList().FindAll(x => x.id_xpTable == findXpTable).Count();
+
+                //l'expérience voulu doit être plus petit que le niveau actuel
+                if (xpVoulu <= 0 && lvlActuel <= 0)
+                {
+                    TempData["errorRechercheUser"] = "xp actuel et lvl voulu sont vide";
+                    return RedirectToAction("Erreur");
+                }
+
+                //l'expérience voulu doit être plus grand que 0
+                else if (xpVoulu < 0)
+                {
+                    //Redirection vers une page d'erreur 
+                    TempData["errorRechercheUser"] = "xp actuel est vide";
+                    return RedirectToAction("Erreur");
+                }
+
+                //Le niveau actuel doit être plus grand que 0
+                else if (lvlActuel < 0)
+                {
+                    //Redirection vers une page d'erreur
+                    TempData["errorRechercheUser"] = "lvl actuel est vide";
+                    return RedirectToAction("Erreur");
+                }
+
+                //Vérification que les niveau actuel et expérience voulu ne sont plus grand que ceux dans la bade de donnée
+                if (xpVoulu <= xpMax && lvlActuel <= lvlMax)
+                {
+                    //Trandformer le niveau en expérience
+                    List<xp> liste = projetFinalEntities1.xps.ToList().FindAll(x => x.lvl >= lvlActuel);
+                    int xpLvl = (int)liste.Find(x => x.id_xpTable == findXpTable).xps;
+
+                    //Le calcul de la différence d'expérience
+                    int DifXp = xpVoulu - xpLvl;
+                    if (DifXp >= 1)
+                    {
+                        //Lors d'une redirection le viewbag devien null
+                        //https://stackoverflow.com/questions/26474579/viewbag-property-not-displaying-in-view
+                        TempData["DifXp"] = DifXp;
+                    }
+                    else
+                    {
+                        //Redirection vers une page d'erreur
+                        TempData["errorRechercheUser"] = "La valeur de xp voulu est plus grand que le lvl de départ";
+                        return RedirectToAction("Erreur");
+                    }
+                }
+                else
+                {
+                    //Redirection vers une page d'erreur
+                    TempData["errorRechercheUser"] = "Les chiffres entres sont trop grand pour les niveaus ou experiences";
+                    return RedirectToAction("Erreur");
+                }
+
+                return RedirectToAction("Calcul", new { id = id });
             }
-            catch(Exception e)
+            catch (Exception e)
             {
+                //Redirection vers une page d'erreur
                 TempData["errorRechercheUser"] = "Désoler un erreur ces produit";
                 return RedirectToAction("Erreur");
             }
         }
 
+        /// <summary>
+        /// GET: Calcul  
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public PartialViewResult Calcul(int id)
-        {          
+        {
+            //la table utilisé pour afficher les résultats du calculateur
             List<competence> listComp = projetFinalEntities1.competences.ToList().FindAll(x => x.id_skillName == id);
             return PartialView(listComp);
         }
 
+        /// <summary>
+        /// GET: Erreur  
+        /// </summary>
+        /// <returns></returns>
         public PartialViewResult Erreur()
         {
+            //Vue partiel pour les erreurs de calcul
             return PartialView();
         }
     }
